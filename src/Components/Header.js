fc3b41d02a0a5a81bfed8aed308fe33a0de75f61
Kiran Kumar V <kirankumarv094@gmail.com>
2023-10-15 13:18:59 +0530

    upadted header css and js files to display the banner

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,10 +1,25 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import bannerImg from "../Assets/restauranfood.jpg";
 
 const Header = () => {
   return (
-    <>
-      <div>Header</div>
-    </>)
+    <header className="header">
+      <div className="banner">
+        <h2>Little Lemon</h2>
+        <h3>Chicago</h3>
+        <p>
+          We are a family-owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.
+        </p>
+        <Link to="/booking">
+          <button aria-label="Reserve a Table">Reserve Table</button>
+        </Link>
+      </div>
+      <div className="banner-img">
+        <img src={bannerImg} alt="Restaurant Banner" />
+      </div>
+    </header>
+  );
 };
 
 export default Header;
